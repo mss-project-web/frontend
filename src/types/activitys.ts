20eb2b6cb955b2f9a80dae8a64d5d2983d1891aa
Lutfee Dorloh <lutfee2017@gmail.com>
2025-07-13 15:43:35 +0700

    add activitys

--- src/types/activitys.ts
+++ src/types/activitys.ts
@@ -8,3 +8,21 @@ export interface Event {
       location: string;
       imageUrl: string;
 }
+
+export interface RoadmapEvent {
+      name: string;
+      date: string;
+      category: string;
+      color: string;
+}
+
+export interface RoadmapMonth {
+      month: string;
+      events: RoadmapEvent[];
+}
+
+export interface RoadmapModalProps {
+      isModalOpen: boolean;
+      handleCloseModal: () => void;
+      roadmapEvents: RoadmapMonth[];
+}
